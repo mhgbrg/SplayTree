d242c9cb222f4194772c22282cf6d5336a63a032
Mats Högberg <mats@hgbrg.se>
2016-03-01 08:37:52 +0100

    Skeleton code for the SimpleSet methods

---
+++ SplayTreeSet.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class SplayTreeSet<E extends Comparable<? super E>> implements SimpleSet<E> {
+
+
+    public int size() {
+        return 0;
+    }
+
+    public boolean add(E x) {
+        return false;
+    }
+
+    public boolean remove(E x) {
+        return false;
+    }
+
+    public boolean contains(E x) {
+        return false;
+    }
+}

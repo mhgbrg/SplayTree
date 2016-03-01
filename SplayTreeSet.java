dd00ae6978016d17943a7569552fe6d89e6c86d3
Mats Högberg <mats@hgbrg.se>
2016-03-01 09:01:21 +0100

    Initial version of find

--- SplayTreeSet.java
+++ SplayTreeSet.java
@@ -49,4 +49,62 @@ public class SplayTreeSet<E extends Comparable<? super E>> implements SimpleSet<
     public boolean contains(E x) {
         return false;
     }
+
+    /**
+     * Finds a node with a specified value in the tree.
+     *
+     * @param x The value to search for.
+     * @return a node from the tree containing the value x. If no such node
+     *         exists, null is returned.
+     */
+    private Node find(E x) {
+        Node node = findRecursive(x, this.root);
+        splay(node);
+
+        // If we ended up at a node with the correct value, we return the node.
+        // Otherwise we return null.
+        if (node != null && node.getValue().equals(x)) {
+            return node;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Recursively finds a node in the tree containing a specified value. If
+     * the specified value is not in the tree, the node where the search ended
+     * is returned.
+     *
+     * @param x The value to search for
+     * @param root The root of a subtree to start the search in.
+     * @return a node containing the value x if such a node exists, otherwise
+     *         the node where the search ended (the last node that wasn't null).
+     *         null is returned if root is null.
+     */
+    private Node findRecursive(E x, Node root) {
+        if (root == null) {
+            return null;
+        }
+
+        int comparison = x.compareTo(root.getValue());
+
+        if (comparison < 0) {
+            if (root.getLeftChild() == null) {
+                return root;
+            }
+            return findRecursive(x, root.getLeftChild());
+        } else if (comparison > 0) {
+            if (root.getRightChild() == null) {
+                return root;
+            }
+            return findRecursive(x, root.getRightChild());
+        } else {
+            // We have found the right node!
+            return root;
+        }
+    }
+
+    private void splay(Node node) {
+        
+    }
 }

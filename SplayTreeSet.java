82fb02618beac3e7dc46edc8044d5e442643cad0
Mats Högberg <mats@hgbrg.se>
2016-03-01 08:38:45 +0100

    Added a private variable for keeping track of the size

--- SplayTreeSet.java
+++ SplayTreeSet.java
@@ -3,15 +3,23 @@ import java.util.*;
 public class SplayTreeSet<E extends Comparable<? super E>> implements SimpleSet<E> {
 
 
+    private int size;
+
+    public SplayTreeSet() {
+        this.size = 0;
+    }
+
     public int size() {
-        return 0;
+        return this.size;
     }
 
     public boolean add(E x) {
+        size++;
         return false;
     }
 
     public boolean remove(E x) {
+        size--;
         return false;
     }
 
